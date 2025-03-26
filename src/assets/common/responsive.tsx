f7e9f2d4f23ca6ab7aebc19923c12d2729aeff8f
Sendega89 <Sendega89@gmail.com>
2025-03-26 15:42:17 +0200

    init in WS

---
+++ src/assets/common/responsive.tsx
@@ -0,0 +1,11 @@
+import { useMediaQuery } from "react-responsive";
+
+export const Desktop = ({ children }: any) => {
+	const isDesktop = useMediaQuery({ minWidth: 551 });
+	return isDesktop ? children : null;
+};
+
+export const Mobile = ({ children }: any) => {
+	const isMobile = useMediaQuery({ maxWidth: 550 });
+	return isMobile ? children : null;
+};
---
+++ src/redux/slices/userSlice.ts
@@ -0,0 +1,16 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { UserState } from "../types/userTypes.ts";
+
+const initialState: UserState = { id: null };
+const userSlice = createSlice({
+	name: "user",
+	initialState,
+	reducers: {
+		setId: (state, action: PayloadAction<number>) => {
+			state.id = action.payload;
+		},
+	},
+});
+
+export const { setId } = userSlice.actions;
+export default userSlice.reducer;
